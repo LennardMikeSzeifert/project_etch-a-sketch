3a5fb517aac8f04311448096521e1aac3ad47177
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-11-27 09:04:13 +0100

    feat(js): implement hover effect
    - Create a function that generates a random background color.
    - Add an event listener to change the background color of each grid square on hover.
    
    Refs: projects/etch-a-sketch/
    Authored-by: Lennard

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 const container = document.querySelector(`.container`);
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement(`div`);
-  div.className = `square-div`;
-  container.appendChild(div);
+function createGrid() {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement(`div`);
+    div.className = `square-div`;
+    container.appendChild(div);
+  }
 }
+createGrid();
+
+const divs = document.querySelectorAll(`.square-div`);
+
+function changeBackgroundColour(e) {
+  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+  e.target.style.backgroundColor = randomColor;
+}
+
+divs.forEach(function (div) {
+  div.addEventListener(`mouseenter`, changeBackgroundColour);
+});
