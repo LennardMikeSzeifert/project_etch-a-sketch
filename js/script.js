da9f37e6e48440b92348ebdc7bb1a042b1a17ff4
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-12-02 14:19:57 +0100

    chore: create empty commit to prepare merge

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const container = document.querySelector(`.container`);
 
 function createGrid() {
   for (let i = 0; i < 256; i++) {
-    const div = document.createElement(`div`);
-    div.className = `square-div`;
-    container.appendChild(div);
+    const grid = document.createElement(`div`);
+    grid.className = `square-div`;
+    container.appendChild(grid);
   }
 }
 createGrid();
@@ -16,39 +16,39 @@ function setUpHoverEffect() {
     )}, ${Math.floor(Math.random() * 256)})`;
     e.target.style.backgroundColor = randomColor;
   }
-  const divs = document.querySelectorAll(`.square-div`);
+  const grids = document.querySelectorAll(`.square-div`);
 
-  divs.forEach((div) => {
-    div.addEventListener(`mouseenter`, changeBackgroundColour);
+  grids.forEach((grid) => {
+    grid.addEventListener(`mouseenter`, changeBackgroundColour);
   });
 }
 setUpHoverEffect();
 const btn = document.querySelector(`.btn`);
 
-function changeGridLayout() {
+function changeGrid() {
   const userGridChoice = prompt(
     `Please enter a number smaller than 100 to define your desired grid layout`
   );
 
   function removeGrid() {
-    const divs = document.querySelectorAll(`.square-div`);
-    divs.forEach((div) => {
-      div.remove();
+    const grids = document.querySelectorAll(`.square-div`);
+    grids.forEach((grid) => {
+      grid.remove();
     });
   }
   removeGrid();
   function createNewGrid() {
     for (let i = 0; i < +userGridChoice * +userGridChoice; i++) {
-      const div = document.createElement(`div`);
-      div.className = `square-div`;
-      container.appendChild(div);
+      const grid = document.createElement(`div`);
+      grid.className = `square-div`;
+      container.appendChild(grid);
       const newFlexBasis = 780 / +userGridChoice;
       const newHeight = 788 / +userGridChoice;
-      div.style.flexBasis = `${newFlexBasis}px`;
-      div.style.height = `${newHeight}px`;
+      grid.style.flexBasis = `${newFlexBasis}px`;
+      grid.style.height = `${newHeight}px`;
     }
     setUpHoverEffect();
   }
   createNewGrid();
 }
-btn.addEventListener(`click`, changeGridLayout);
+btn.addEventListener(`click`, changeGrid);
