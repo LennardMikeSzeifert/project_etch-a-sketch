56446b368364058b9a9ef644493fca9994bb8d2b
codeandtranslate <123456789+translatetodeutsch@gmail.com>
2025-12-02 14:14:10 +0100

    feat(js,css): add event listener to change the grid
    Including:
    - Build a function that removes the current grid.
    - Add a prompt asking the user for a number to create a custom grid size.
    - Calculate new flex-basis and height values based on the user input.
    - Add a function that creates a new grid using the chosen size.
    - Reuse the existing hover-effect function for the new grid.
    
    Refs: projects/etch-a-sketch/
    Authored-by: Lennard

--- js/script.js
+++ js/script.js
@@ -9,15 +9,46 @@ function createGrid() {
 }
 createGrid();
 
-const divs = document.querySelectorAll(`.square-div`);
+function setUpHoverEffect() {
+  function changeBackgroundColour(e) {
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+    e.target.style.backgroundColor = randomColor;
+  }
+  const divs = document.querySelectorAll(`.square-div`);
 
-function changeBackgroundColour(e) {
-  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )}, ${Math.floor(Math.random() * 256)})`;
-  e.target.style.backgroundColor = randomColor;
+  divs.forEach((div) => {
+    div.addEventListener(`mouseenter`, changeBackgroundColour);
+  });
 }
+setUpHoverEffect();
+const btn = document.querySelector(`.btn`);
 
-divs.forEach(function (div) {
-  div.addEventListener(`mouseenter`, changeBackgroundColour);
-});
+function changeGridLayout() {
+  const userGridChoice = prompt(
+    `Please enter a number smaller than 100 to define your desired grid layout`
+  );
+
+  function removeGrid() {
+    const divs = document.querySelectorAll(`.square-div`);
+    divs.forEach((div) => {
+      div.remove();
+    });
+  }
+  removeGrid();
+  function createNewGrid() {
+    for (let i = 0; i < +userGridChoice * +userGridChoice; i++) {
+      const div = document.createElement(`div`);
+      div.className = `square-div`;
+      container.appendChild(div);
+      const newFlexBasis = 780 / +userGridChoice;
+      const newHeight = 788 / +userGridChoice;
+      div.style.flexBasis = `${newFlexBasis}px`;
+      div.style.height = `${newHeight}px`;
+    }
+    setUpHoverEffect();
+  }
+  createNewGrid();
+}
+btn.addEventListener(`click`, changeGridLayout);
